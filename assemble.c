ac8d786eb6e63d93d1b18b97c8c631812afebee5
Kevin Hicks <keyholesoftware@gmail.com>
2015-03-11 23:47:14 -0400

    Update

---
+++ assemble.c
@@ -0,0 +1,191 @@
+/*
+ * assemble.c
+ *
+ *  Created on: Mar 11, 2015
+ *      Author: HICKSK
+ */
+
+#include "stddef.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "ctype.h"
+#include "string.h"
+
+#include "assemble.h"
+#include "parser.h"
+#include "directives.h"
+
+//Psuedocode
+//Pass 0
+//	Read each line of input file until EOF
+//	If line contains MACRO
+//		Read & store in lok up table
+//	rewind to start of input file
+//
+//Pass 1
+//	Read each line of input file until EOF
+//		If macro, expand.
+//		If variable, store in lookup table
+//		Count bytes of output
+//	Output to temp file
+//	rewind to start of input file
+//
+//Pass 2
+//	Read each line of temp file until EOF
+//		Assemble line
+//		output binary to output file
+//		output listing to l
+
+void beginAssembly(ASSEMBLECONTEXT* context) {
+
+	context->unifiedFile->file = fopen("testFiles\\test.uni", "w+");
+	context->interFile->file = fopen("testFiles\\test.int", "w+");
+	context->listingFile->file = fopen("testFiles\\test.lst", "w+");
+	context->outputFile->file = fopen("testFiles\\test.out", "w+");
+
+	context->firstLabelEntry = NULL;
+	context->firstMacroEntry = NULL;
+	context->fileDepth = 1;
+}
+
+void endAssembly(ASSEMBLECONTEXT* context) {
+	fclose(context->unifiedFile->file);
+	fclose(context->interFile->file);
+	fclose(context->listingFile->file);
+	fclose(context->outputFile->file);
+}
+
+void assembleFile(ASSEMBLECONTEXT* assembleContext, char* fileName) {
+	FILECONTEXT newFileContext;
+
+	newFileContext.file = fopen(fileName, "r");
+	newFileContext.lineNumber = 0;
+	newFileContext.charNumber = 1;
+
+	//Point the new filesContexts parent to the previous file
+	newFileContext.parentFile = assembleContext->currFile;
+
+	//And set the newFileContext as the current file
+	assembleContext->currFile = &newFileContext;
+
+	printf("Pass 0\n");
+	pass0(assembleContext); //Read & Store Macros, symbolic constants, and handle includes
+
+	printf("Pass 1\n");
+	pass1(assembleContext);	//Expand Macros & Count Instructions
+
+	printf("Pass 2\n");
+	pass2(assembleContext);	//Assemble & Output
+
+	fclose(newFileContext.file);
+
+	//revert back to the previous parent file, so that processing may continue
+	assembleContext->currFile = newFileContext.parentFile;
+
+	printf("Assemble Complete: %s\n", fileName);
+}
+
+void pass0(ASSEMBLECONTEXT* context) {
+
+	//Prime pump
+	readCharIntoBuffer(context);
+	while (context->currFile->lineBufferLookAhead != EOF) {
+		readLineIntoBuffer(context);
+
+		if (context->currFile->lookAhead != '\0') {
+
+			/*
+			 readIdent(context);
+			 if (context->currFile->lookAhead == ':') {
+			 //THis is a label
+			 addLabel(context, context->currFile->tokenBuffer);
+			 }
+			 int directive = findDirective(context->currFile->tokenBuffer);
+
+			 if (directive != DRTV_NOT_FOUND) {
+			 doDirective(directive, context);
+			 }
+			 */
+		}
+
+		printf("%s\n", context->currFile->lineBuffer);
+		fprintf(context->unifiedFile->file, "%s\n", context->currFile->lineBuffer);
+		//Look For Define, or MACRO
+	}
+}
+
+void pass1(ASSEMBLECONTEXT* context) {
+
+	context->currFile->file = context->unifiedFile->file;
+
+	context->currFile->lineNumber = 0;
+	context->currFile->charNumber = 1;
+	context->outputPos = 0;
+
+	rewind(context->currFile->file);
+	//Prime pump
+	readCharIntoBuffer(context);
+	while (context->currFile->lineBufferLookAhead != EOF) {
+		readLineIntoBuffer(context);
+
+		int pos = context->outputPos;
+
+		//A non-empty line
+		if (context->currFile->lookAhead != '\0') {
+
+			doInstruction();
+
+			//read the first label/instruction/macro etc
+			readIdent(context);
+			if (context->currFile->lookAhead == ':') {
+				//This is a label
+				addLabel(context, context->currFile->tokenBuffer);
+			} else {
+				//Try to assemble it
+				doInstruction(context);
+			}
+
+
+		}
+		fprintf(context->interFile->file, "%08x  %s\n", pos, context->currFile->lineBuffer);
+		//Look For Define, or MACRO
+	}
+}
+
+void pass2(ASSEMBLECONTEXT* context) {
+	context->outputPos = 0;
+
+	emitLabelsToListing(context);
+}
+
+void emitLabelsToListing(ASSEMBLECONTEXT* context) {
+	fprintf(context->listingFile->file, "LABELS:\n");
+
+	LABELTABLEENTRY* head = context->firstLabelEntry;
+
+	while (head != NULL) {
+		fprintf(context->listingFile->file, "%-10s %d\n", head->name, head->position);
+
+		head = head->nextEntry;
+	}
+}
+
+void addLabel(ASSEMBLECONTEXT* context, char* labelName) {
+	LABELTABLEENTRY* tail = context->firstLabelEntry;
+
+	if (tail == NULL) {
+		context->firstLabelEntry = malloc(sizeof(LABELTABLEENTRY));
+		tail = context->firstLabelEntry;
+	} else {
+		while (tail->nextEntry != NULL) {
+			tail = tail->nextEntry;
+		}
+		tail->nextEntry = malloc(sizeof(LABELTABLEENTRY));
+		tail = tail->nextEntry;
+	}
+
+	strcpy(tail->name, labelName);
+	tail->position = context->outputPos;
+	tail->nextEntry = NULL;
+}
+
---
+++ assemble.h
@@ -0,0 +1,88 @@
+/*
+ * assemble.h
+ *
+ *  Created on: Mar 11, 2015
+ *      Author: HICKSK
+ */
+
+#ifndef ASSEMBLE_H_
+#define ASSEMBLE_H_
+
+
+#include "stdio.h"
+#include "constants.h"
+
+typedef struct MACROTABLEENTRY MACROTABLEENTRY;
+typedef struct ASSEMBLECONTEXT ASSEMBLECONTEXT;
+typedef struct LABELTABLEENTRY LABELTABLEENTRY;
+typedef struct FILECONTEXT FILECONTEXT;
+
+//The information about the entire assembling process
+struct ASSEMBLECONTEXT {
+	int fileDepth;				//How deep in recursion are we assembling files
+
+	FILECONTEXT* currFile; 		//The file we are currently assembling. This points to the tail of a liked list of files
+
+	FILECONTEXT* unifiedFile;	//The result of pass0
+	FILECONTEXT* interFile;		//The result of pass1
+	FILECONTEXT* outputFile;	//The result of pass2
+	FILECONTEXT* listingFile;	//Human readable output of pass2
+
+	MACROTABLEENTRY* firstMacroEntry;	//Points to the head of a linked list of macros
+	LABELTABLEENTRY* firstLabelEntry;	//Points to the head of a linked list of labels
+
+	int outputPos;				//The position in the output binary
+};
+
+//Entry in linked list of Macros
+struct MACROTABLEENTRY {
+	MACROTABLEENTRY* nextEntry;	//Points to the next entry in a linked list
+	char* name;					//The defined name of the macro
+	char* text;					//What the macro represents
+};
+
+//Entry in linked list of Labels
+struct LABELTABLEENTRY {
+	LABELTABLEENTRY* nextEntry;	//Points to the next entry in a linked list
+	int size;					//
+	char name[MAX_TOKEN_SIZE];					//The defined name of the label
+	int type;					//
+	int position;				//The location the label is located
+};
+
+//Entry in linked list of files
+struct FILECONTEXT {
+	FILE* file;							//The system reference to the file
+	FILECONTEXT* parentFile;			//Points to the previous file we were assembling before we started this one
+	int lineNumber;						//The lineNumber we are currently assembling
+	int charNumber;						//The column of the lookAhead char
+	char lookAhead;						//A single char giving us a preveiw of whats ahead of the the current token in the lineBuffer.
+	int lookAheadPosition;				//The position in the lineBuffer that the lookAhead is at
+	char tokenBuffer[MAX_TOKEN_SIZE];	//The current token/identifier/number
+
+	char lineBufferLookAhead;			//The look ahead character looking at the next character from the file
+	char lineBuffer[MAX_LINE_BUFFER_SIZE];	//The text of the line we are currently assembling
+
+	int inseideQuotes;
+};
+
+//Prepare for assembly
+void beginAssembly(ASSEMBLECONTEXT* context);
+//Finsih assembly. Close files, and cleanup
+void endAssembly(ASSEMBLECONTEXT* context);
+
+//Assemble the file
+void assembleFile(ASSEMBLECONTEXT* assembleContext, char* fileName);
+//Perform Pass0. Looking for Preprocessor directives
+void pass0(ASSEMBLECONTEXT* context);
+//Perform Pass1. Counting instruction sizes, and collecting labels
+void pass1(ASSEMBLECONTEXT* context);
+//Perform Pass2. Assemble instructions, output to output file & listing file
+void pass2(ASSEMBLECONTEXT* context);
+
+//Print all the labels, and there positions to the listings file
+void emitLabelsToListing(ASSEMBLECONTEXT* context);
+//Add a label to the Label Table
+void addLabel(ASSEMBLECONTEXT* context, char* labelName);
+
+#endif /* ASSEMBLE_H_ */
